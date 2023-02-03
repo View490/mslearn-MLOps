9d3af5a797c5b8dc1b1a00bbbe371c9660df6a02
View490 <114554173+View490@users.noreply.github.com>
2023-02-03 12:35:26 +0700

    Add the "split_data(df)" function.

--- src/model/train.py
+++ src/model/train.py
@@ -3,6 +3,8 @@
 import argparse
 import glob
 import os
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 import pandas as pd
 
@@ -56,6 +58,12 @@ def parse_args():
 
     # return args
     return args
+
+def split_data(df):
+    X = df.iloc[:, :-1]
+    y = df.iloc[:, -1]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    return X_train, X_test, y_train, y_test
 
 # run script
 if __name__ == "__main__":
